3086b770e58b43a5c6dde5b8ceffc4990cafeb0a
Roman Prokopchuk <ghost62727@gmail.com>
2023-12-01 18:36:05 +0200

    Add RSQL builder, refactor retrieving cars page (#9)

---
+++ src/app/services/rsql/rsql-operators.ts
@@ -0,0 +1,13 @@
+export module RsqlOperators {
+    export const EQUALS: string = '==';
+    export const NOT_EQUALS: string = '!=';
+    export const LESS_THAN: string = '=lt=';
+    export const LESS_THAN_OR_EQUAL: string = '=le=';
+    export const GREATER_THAN: string = '=gt=';
+    export const GREATER_THAN_OR_EQUAL: string = '=ge=';
+    export const IN: string = '=in=';
+    export const OUT: string = '=out=';
+
+    export const AND: string = ';';
+    export const OR: string = ',';
+}
